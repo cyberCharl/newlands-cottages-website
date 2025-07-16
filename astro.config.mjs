2a07513f69e9183246458780a31519f3325d6b2e
Charl Botha <chrl.botha24@gmail.com>
2025-07-16 18:04:20 +0200

    added tailwind and adjusted paths to root relative

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    site: 'https://cyberCharl.github.io',
-    base: '/newlands-cottages-website',
-  });
+  site: 'https://cyberCharl.github.io',
+  base: '/newlands-cottages-website/',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
