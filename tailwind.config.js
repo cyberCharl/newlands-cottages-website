47c9ec0e08d3234f5cfa44b2dd12f7f1eef3e16f
Charl Botha <chrl.botha24@gmail.com>
2025-07-17 13:44:33 +0200

    working flowbite slider, modal still broken

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./src/**/*.{astro,html,js,ts,jsx,tsx,vue,svelte}",
+      "./node_modules/flowbite/**/*.js",
+    ],
+    theme: { extend: {} },
+    plugins: [require("flowbite/plugin")],
+  };
